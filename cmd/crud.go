e57a52a89b5cecc9957bfd5ddc12ab86ce74b669
Brian Lehrer <661570+blehrer@users.noreply.github.com>
2025-07-15 17:57:31 -0700

    Refactor some crud methods

--- cmd/crud.go
+++ cmd/crud.go
@@ -11,20 +11,112 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func chooseHttpMethod(path string) (string, error) {
+	var method string
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("Path").
+				Value(&path),
+			huh.NewSelect[string]().
+				Title("Method").
+				Options(
+					huh.NewOption("GET", "GET"),
+					huh.NewOption("POST", "POST"),
+					huh.NewOption("PUT", "PUT"),
+					huh.NewOption("DELETE", "DELETE"),
+					huh.NewOption("PATCH", "PATCH"),
+					huh.NewOption("HEAD", "HEAD"),
+					huh.NewOption("OPTIONS", "OPTIONS"),
+				).Value(&method)),
+	)
+
+	var err = form.Run()
+	return method, err
+}
+
+func createEndpoint(document libopenapi.Document, model libopenapi.DocumentModel[v3.Document], filepath string) {
+	var path string
+	var newSummary, newOperationId, newDescription string
+
+	httpMethod, err := chooseHttpMethod(path)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	editForm := createEditForm(&newSummary, &newOperationId, &newDescription)
+	err = editForm.Run()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	// Create a new path item if it doesn't exist
+	pathItem := model.Model.Paths.PathItems.GetOrZero(path)
+	if pathItem == nil {
+		pathItem = &v3.PathItem{}
+		model.Model.Paths.PathItems.Set(path, pathItem)
+	}
+
+	// Create a new operation
+	operation := &v3.Operation{
+		Summary:     newSummary,
+		OperationId: newOperationId,
+		Description: newDescription,
+	}
+	switch httpMethod {
+	case "GET":
+		pathItem.Get = operation
+	case "POST":
+		pathItem.Post = operation
+	case "PUT":
+		pathItem.Put = operation
+	case "DELETE":
+		pathItem.Delete = operation
+	case "PATCH":
+		pathItem.Patch = operation
+	case "HEAD":
+		pathItem.Head = operation
+	case "OPTIONS":
+		pathItem.Options = operation
+	}
+
+	// Save the updated model to openapi.yaml
+	rendered, err := document.Render()
+	if err != nil {
+		fmt.Printf("Error rendering document: %s\n", err)
+		return
+	}
+	err = os.WriteFile(filepath, rendered, 0644)
+	if err != nil {
+		fmt.Printf("Error writing to %s: %s\n", filepath, err)
+		return
+	}
+	fmt.Println("Endpoint created successfully!")
+}
+
 var crudCmd = &cobra.Command{
-	Use:   "crud",
-	Short: "CRUD an OpenAPI spec",
-	Long:  `This command opens a wizard to perform CRUD operations on an OpenAPI spec.`,
+	Use:           `crud`,
+	Short:         `CRUD an OpenAPI spec`,
+	Long:          `This command opens a wizard to perform CRUD operations on an OpenAPI spec.`,
+	Args:          cobra.RangeArgs(0, 1),
+	Example:     " crudr crud\n crudr crud path/to/spec.yaml",
+	ValidArgs: []cobra.Completion{cobra.CompletionWithDesc(`filename`, `defaults to ./openapi.yaml`)},
 	Run: func(cmd *cobra.Command, args []string) {
-		openapiBytes, err := os.ReadFile("openapi.yaml")
+		var filepath = "openapi.yaml"
+		if len(args) == 1 {
+			filepath = args[0]
+		}
+
+		openapiBytes, err := os.ReadFile(filepath)
 		if err != nil {
-			fmt.Println("No openapi.yaml found. Please run 'gocrudr init' first.")
+			fmt.Printf("%s found. Please run 'gocrudr init' first.\n", filepath)
 			return
 		}
 
 		document, err := libopenapi.NewDocument(openapiBytes)
 		if err != nil {
-			fmt.Printf("Error parsing openapi.yaml: %s\n", err)
+			fmt.Printf("Error parsing %s: %s\n", filepath, err)
 
 			return
 		}
@@ -32,8 +124,8 @@ var crudCmd = &cobra.Command{
 		model, _ := document.BuildV3Model()
 
 		paths := make([]string, 0, (model.Model.Paths.PathItems.Len()))
-		for pair := model.Model.Paths.PathItems.First(); pair != nil; pair = pair.Next() {
-			paths = append(paths, pair.Key())
+		for k := range model.Model.Paths.PathItems.OrderedMap.KeysFromNewest() {
+			paths = append(paths, k)
 		}
 		sort.Strings(paths)
 
@@ -59,85 +151,7 @@ var crudCmd = &cobra.Command{
 		}
 
 		if selectedEndpoint == "+ New Endpoint" {
-			var path, method string
-			var newSummary, newOperationId, newDescription string
-
-			form := huh.NewForm(
-				huh.NewGroup(
-					huh.NewInput().
-						Title("Path").
-						Value(&path),
-					huh.NewSelect[string]().
-						Title("Method").
-						Options(
-							huh.NewOption("GET", "GET"),
-							huh.NewOption("POST", "POST"),
-							huh.NewOption("PUT", "PUT"),
-							huh.NewOption("DELETE", "DELETE"),
-							huh.NewOption("PATCH", "PATCH"),
-							huh.NewOption("HEAD", "HEAD"),
-							huh.NewOption("OPTIONS", "OPTIONS"),
-						).Value(&method)),
-			)
-
-			err = form.Run()
-			if err != nil {
-				fmt.Printf("Error: %v\n", err)
-				return
-			}
-
-			editForm := createEditForm(&newSummary, &newOperationId, &newDescription)
-			err = editForm.Run()
-			if err != nil {
-				fmt.Printf("Error: %v\n", err)
-				return
-			}
-
-			// Create a new path item if it doesn't exist
-			pathItem := model.Model.Paths.PathItems.GetOrZero(path)
-			if pathItem == nil {
-				pathItem = &v3.PathItem{}
-				model.Model.Paths.PathItems.Set(path, pathItem)
-			}
-
-			// Create a new operation
-			operation := &v3.Operation{
-				Summary:     newSummary,
-				OperationId: newOperationId,
-				Description: newDescription,
-			}
-
-			switch method {
-			case "GET":
-				pathItem.Get = operation
-			case "POST":
-				pathItem.Post = operation
-			case "PUT":
-				pathItem.Put = operation
-			case "DELETE":
-				pathItem.Delete = operation
-			case "PATCH":
-				pathItem.Patch = operation
-			case "HEAD":
-				pathItem.Head = operation
-			case "OPTIONS":
-				pathItem.Options = operation
-			}
-
-			// Save the updated model to openapi.yaml
-			rendered, err := document.Render()
-			if err != nil {
-				fmt.Printf("Error rendering document: %s\n", err)
-				return
-			}
-
-			err = os.WriteFile("openapi.yaml", rendered, 0644)
-			if err != nil {
-				fmt.Printf("Error writing to openapi.yaml: %s\n", err)
-				return
-			}
-
-			fmt.Println("Endpoint created successfully!")
+			createEndpoint(document, *model, filepath)
 		} else {
 			pathItem := model.Model.Paths.PathItems.GetOrZero(selectedEndpoint)
 
@@ -215,9 +229,9 @@ var crudCmd = &cobra.Command{
 				return
 			}
 
-			err = os.WriteFile("openapi.yaml", rendered, 0644)
+			err = os.WriteFile(filepath, rendered, 0644)
 			if err != nil {
-				fmt.Printf("Error writing to openapi.yaml: %s\n", err)
+				fmt.Printf("Error writing to %s: %s\n", filepath, err)
 				return
 			}
 
